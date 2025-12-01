f0d5401032f1bc5c14a8be71bdc8f4178096209e
Bawa Ernest Kwame <bawaernest926@gmail.com>
2025-12-01 20:11:59 +0000

    update update

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,76 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Pages from "./pages/Pages";
+import Branches from "./pages/Branches";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Footer from "./Components/Footer";
 
-// Components
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
-import ScrollToTop from "./components/ScrollToTop.jsx";
-import DrIgnatus from "./components/DrIgnatus.jsx";
+// 🩵 Import About components (correct folder)
+import HospitalOverview from "./pages/AboutComponents/HospitalOverview";
+import WhoWeAre from "./pages/AboutComponents/WhoWeAre";
+import Mission from "./pages/AboutComponents/Mission";
+import Awards from "./pages/AboutComponents/Awards";
+import Experience from "./pages/AboutComponents/Experience";
+import SuccessStory from "./pages/AboutComponents/SuccessStory";
+import Blog from "./Pages/Blog";
+import DrIgnatus from "./Components/DrIgnatus";
+import Berekum from './Pages/Berekum';
+import Dormaa from './Pages/Dormaa';
+import Glaucoma from './Pages/serviceList/Glaucoma';
+import DvlEyeTeste from './Pages/serviceList/DvlEyeTeste';
+import EyeSurgeries from './Pages/serviceList/EyeSurgeries';
+import Refraction from './Pages/serviceList/Refraction';
+import InstitutionalEye from './Pages/serviceList/InstitutionalEye';
+import Team from "./pages/Team";
+import ScrollToTop from "./Components/ScrollToTop";
 
-// Pages
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import Pages from "./pages/Pages.jsx";
-import Branches from "./pages/Branches.jsx";
-import Services from "./pages/Services.jsx";
-import Contact from "./pages/Contact.jsx";
-import Blog from "./pages/Blog.jsx";
-import Berekum from "./pages/Berekum.jsx";
-import Dormaa from "./pages/Dormaa.jsx";
-import Team from "./pages/Team.jsx";
-
-// About components
-import HospitalOverview from "./pages/AboutComponents/HospitalOverview.jsx";
-import WhoWeAre from "./pages/aboutcomponents/WhoWeAre.jsx";
-import Mission from "./pages/aboutcomponents/Mission.jsx";
-import Awards from "./pages/aboutcomponents/Awards.jsx";
-import Experience from "./pages/aboutcomponents/Experience.jsx";
-import SuccessStory from "./pages/aboutcomponents/SuccessStory.jsx";
-
-// Service list components
-import Glaucoma from "./pages/servicelist/Glaucoma.jsx";
-import DvlEyeTeste from "./pages/servicelist/DvlEyeTeste.jsx";
-import EyeSurgeries from "./pages/servicelist/EyeSurgeries.jsx";
-import Refraction from "./pages/servicelist/Refraction.jsx";
-import InstitutionalEye from "./pages/servicelist/InstitutionalEye.jsx";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <ScrollToTop />
-      <Navbar />
+   
+   
+      <div className="min-h-screen flex flex-col">
+         <ScrollToTop />
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        {/* About section with nested routes */}
-        <Route path="/about" element={<About />}>
-          <Route index element={<WhoWeAre />} />
-          <Route path="mission" element={<Mission />} />
-          <Route path="awards" element={<Awards />} />
-          <Route path="experience" element={<Experience />} />
-          <Route path="success" element={<SuccessStory />} />
+          {/* 🩵 About section with nested routes */}
+          <Route path="/about" element={<About />}>
+            <Route index  element={<WhoWeAre />} />
+            <Route path="mission" element={<Mission />} />
+            <Route path="awards" element={<Awards />} />
+            <Route path="experience" element={<Experience />} />
+            <Route path="success" element={<SuccessStory />} />
+          </Route>
+
+         <Route path="/services" element={<Services/>}>
+          <Route index element={<Glaucoma/>} />
+          <Route path="dvlEyeTeste" element={<DvlEyeTeste/>} />
+          <Route path="eyeSurgeries" element={<EyeSurgeries/>} />
+          <Route path="refraction" element={<Refraction/>} />
+          <Route path="institutionalEye" element={<InstitutionalEye/>} />
         </Route>
 
-        {/* Services section with nested routes */}
-        <Route path="/services" element={<Services />}>
-          <Route index element={<Glaucoma />} />
-          <Route path="dvleyeTeste" element={<DvlEyeTeste />} />
-          <Route path="eyesurgeries" element={<EyeSurgeries />} />
-          <Route path="refraction" element={<Refraction />} />
-          <Route path="institutionaleye" element={<InstitutionalEye />} />
-        </Route>
 
-        <Route path="/pages" element={<Pages />} />
-        <Route path="/branches" element={<Branches />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/drIgnatus" element={<DrIgnatus />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/dormaa" element={<Dormaa />} />
-        <Route path="/berekum" element={<Berekum />} />
-      </Routes>
+          <Route path="/pages" element={<Pages />} />
+          <Route path="/branches" element={<Branches />} />
+          <Route path="/blog" element={<Blog/>} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/drIgnatus" element={<DrIgnatus/> } />
+          <Route path="/team" element={<Team/> } />
+          <Route path="/dormaa" element={<Dormaa/> } />
+          <Route path="/berekum" element={<Berekum/> } />
+        </Routes>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    
   );
 };
 
